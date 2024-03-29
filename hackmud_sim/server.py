823ce8b2e7e75e9d097f8404f034bc9909e01775
dayofni <98456413+dayofni@users.noreply.github.com>
2024-03-29 19:14:37 +1030

    getting channels set up

--- hackmud_sim/server.py
+++ hackmud_sim/server.py
@@ -1,7 +1,8 @@
 
 import asyncio
+import re
 
-from time import time as unix_secs # yeah look I probably shouldn't do this but it's *very useful*
+from time import time as unix_secs # yeah look I probably shouldn't rename it but ya know it's fine
 
 from hackmud_sim.db      import HackmudDatabase
 from hackmud_sim.sockets import WebsocketHandler
@@ -18,9 +19,9 @@ class HackmudServer:
         self.ws      = WebsocketHandler()
         self.logger  = setup_logger(name=__name__)
         
-        self.clients  = {}
-        self.users    = {}
-        self.channels = {}
+        self.clients      = {}
+        self.users        = {}
+        self.channels     = {"0000": []}
     
     async def __aenter__(self):
         self.db.__enter__()
@@ -37,18 +38,62 @@ class HackmudServer:
 
     async def __aexit__(self, *exc):
         self.db.__exit__(*exc)
+        
+    # Users
+    
+    def create_user(self, user):
+        
+        self.users[user] = {
+            "channels": [
+                "0000"
+            ]
+        }
+        
+        self.channels["0000"].append(user)
+        
+    
+    def delete_user(self, user):
+        ...
+    
+    # Channel list
+    
+    def create_channel(self, channel_name):
+        
+        self.channels[channel_name] = []
+    
+    def join_channel(self, user, channel_name):
+        
+        self.users[user]["channels"].append(channel_name)
+        self.channels[channel_name].append(user)
+    
+    def leave_channel(self, user, channel_name):
+        ...
     
     # Websocket connectivity
     
     async def websocket_join(self, user):
-        ...
+        self.users[user] = {"channels": "0000"}
+        self.channels["0000"].append(user)
         
     async def websocket_msg(self, user, message):
         
-        msg = f"{user[:4]}: {message}"
-        await self.ws.broadcast_msg(list(self.ws.sockets.keys()), msg)
+        # commands
+        # - sub <channel>
+        # - unsub
+        # - join
+        # -  create
+        # - standard (send message to 0000)
         
-        self.logger.info(msg)
+        command = message.split()[0]
+        if command not in ["sub", "unsub", "create"]:
+            command = "send"
+        else:
+            message = message.split(" ")[1:]
+        
+        if command == "sub":
+            
+        
+        ...
     
     async def websocket_exit(self, user):
         ...
